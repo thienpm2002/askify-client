79d52ce6cc03db88501372ec8b466f35e6944159
Thien <ledinhthienpm@gmail.com>
2026-05-04 17:32:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button }   from '@/components/ui/button'
+
+function App() {
+  
+
+  return (
+    <div className="app flex flex-col justify-center items-center h-screen">
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
